a76eb83982efb67f86cb57c4c5ebb73ada6d32d4
ferasinka <iam@ferasinka.com>
2017-01-01 20:40:45 +0300

    InMemoryAdminRestControllerTest fixed

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerTest.java
@@ -16,40 +16,42 @@ import static ru.javawebinar.topjava.UserTestData.ADMIN;
 import static ru.javawebinar.topjava.UserTestData.USER;
 
 public class InMemoryAdminRestControllerTest {
-    private static ConfigurableApplicationContext appCtx;
-    private static AdminRestController controller;
-
-    @BeforeClass
-    public static void beforeClass() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
-        controller = appCtx.getBean(AdminRestController.class);
-    }
-
-    @AfterClass
-    public static void afterClass() {
-        appCtx.close();
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        // Re-initialize
-        UserRepository repository = appCtx.getBean(UserRepository.class);
-        repository.getAll().forEach(u -> repository.delete(u.getId()));
-        repository.save(USER);
-        repository.save(ADMIN);
-    }
-
-    @Test
-    public void testDelete() throws Exception {
-        controller.delete(UserTestData.USER_ID);
-        Collection<User> users = controller.getAll();
-        Assert.assertEquals(users.size(), 1);
-        Assert.assertEquals(users.iterator().next(), ADMIN);
-    }
-
-    @Test(expected = NotFoundException.class)
-    public void testDeleteNotFound() throws Exception {
-        controller.delete(10);
-    }
+	private static ConfigurableApplicationContext appCtx;
+	private static AdminRestController controller;
+	
+	@BeforeClass
+	public static void beforeClass() {
+		appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+		
+		System.out.println("\n" + Arrays.toString(appCtx.getBeanDefinitionNames()) + "\n");
+		
+		controller = appCtx.getBean(AdminRestController.class);
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		appCtx.close();
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		// Re-initialize
+		UserRepository repository = appCtx.getBean(UserRepository.class);
+//		repository.getAll().forEach(u -> repository.delete(u.getId()));
+		repository.save(USER);
+		repository.save(ADMIN);
+	}
+	
+	@Test
+	public void testDelete() throws Exception {
+		controller.delete(UserTestData.USER_ID);
+		Collection<User> users = controller.getAll();
+		Assert.assertEquals(users.size(), 1);
+		Assert.assertEquals(users.iterator().next(), ADMIN);
+	}
+	
+	@Test(expected = NotFoundException.class)
+	public void testDeleteNotFound() throws Exception {
+		controller.delete(10);
+	}
 }
