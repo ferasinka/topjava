2140ad522c3489d49e1987fcb95f2171258a24b3
ferasinka <iam@ferasinka.com>
2017-02-07 21:10:59 +0300

    Empty testMeals() added

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -9,25 +9,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
-/**
- * GKislin
- * 10.04.2015.
- */
 public class RootControllerTest extends AbstractControllerTest {
-
-    @Test
-    public void testUsers() throws Exception {
-        mockMvc.perform(get("/users"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("users"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
-                .andExpect(model().attribute("users", hasSize(2)))
-                .andExpect(model().attribute("users", hasItem(
-                        allOf(
-                                hasProperty("id", is(START_SEQ)),
-                                hasProperty("name", is(USER.getName()))
-                        )
-                )));
-    }
+	@Test
+	public void testUsers() throws Exception {
+		mockMvc.perform(get("/users"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(view().name("users"))
+				.andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+				.andExpect(model().attribute("users", hasSize(2)))
+				.andExpect(model().attribute("users", hasItem(
+						allOf(
+								hasProperty("id", is(START_SEQ)),
+								hasProperty("name", is(USER.getName()))
+						)
+				)));
+	}
+	
+	@Test
+	public void testMeals() throws Exception {
+		
+	}
 }
